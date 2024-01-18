27c1bdf4a220056e2372dfe784b977a2975df209
Vansh16aug <kumarvansh16aug@gmail.com>
2024-01-18 09:42:39 +0530

    Pratice on operators

---
+++ 18-01/operator.js
@@ -0,0 +1,124 @@
+// let a=10;
+// ++a;
+// a++;
+// --a;
+// a--;
+// console.log(a);
+
+
+
+let x=5;
+let y=6;
+let z=10;
+
+// 1.
+console.log(x+y);
+
+//2.
+console.log(x-y);
+console.log(y-x);
+
+//3.
+console.log(x*y);
+
+// 4.
+console.log(x/y);
+console.log(y/x);
+
+//5.
+console.log(x%y);
+
+//6.
+console.log(x**y);
+
+//7.
+console.log(x**y);
+
+// 8.
+console.log(--z);
+
+
+let a = 20;
+let b = 30;
+let c = 40;
+let d = 50;
+
+// 9.
+console.log((a += a));
+
+//10.
+console.log((b -= b));
+
+// 11.
+console.log(c *= c);
+
+//12.
+console.log(d /= d);
+
+
+// 13.
+//Strict Equality
+
+if(a === b){
+    console.log('True');
+}
+else{
+    console.log('False');
+}
+
+
+// 14.
+//Strict Non-Equality
+
+if (a !== b) {
+  console.log("True");
+} 
+else {
+  console.log("False");
+}
+
+
+// 15.
+//Less Than
+
+if (a < b) {
+  console.log("True");
+} 
+else {
+  console.log("False");
+}
+
+
+// 16.
+//Greater Than
+
+if (a > b) {
+  console.log("True");
+} 
+else {
+  console.log("False");
+}
+
+
+// 17.
+//Less than or equal to
+
+if (a <= b) {
+  console.log("True");
+} 
+else {
+  console.log("False");
+}
+
+
+// 18.
+//Greater than or equal to
+
+if (a >= b) {
+  console.log("True");
+} 
+else {
+  console.log("False");
+}
+
+
