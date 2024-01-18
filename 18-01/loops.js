b60ca5c78702e71f7f23aa583848bcd00056c0dd
Vansh16aug <kumarvansh16aug@gmail.com>
2024-01-18 09:49:39 +0530

    Pratice of loops

---
+++ 18-01/loops.js
@@ -0,0 +1,25 @@
+// 1.
+let age=12;
+console.log(10<age<20?'True':'False');
+
+// 2.
+let hi = 1;
+switch (hi) {
+  case 1:
+    console.log("It is equal to 1");
+    break;
+  case 2:
+    console.log("It is equal to 2");
+  default:
+    break;
+}
+
+// 3.
+
+let a=2;
+console.log(a % 2 == 0 && a % 3 == 0 ? "True" : "False");
+
+// 4.
+
+let b = 2;
+console.log(b % 2 == 0 || b % 3 == 0 ? "True" : "False");
