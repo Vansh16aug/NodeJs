c3a94891cc7556f930fb6c8cd820b3240a8d11c8
Vansh16aug <kumarvansh16aug@gmail.com>
2024-01-22 10:10:22 +0530

    Did practise on loops and functions

---
+++ 22-01/function.js
@@ -0,0 +1,27 @@
+// // 1.
+// function ageCalculator(birthYear,presentYear){
+//     let age=presentYear-birthYear;
+//     console.log("The age is :", age);
+//     // console.log(age);
+// }
+// ageCalculator(2005,2024);
+
+
+// // 2.
+// function Addition(a,b){
+//     let ans=a+b;
+//     console.log(ans);
+// }
+// Addition(2,3);
+
+
+// // 3.
+// console.log(x);
+// var x="I can access it";
+// console.log(x);
+
+// 4.
+add=(a,b)=>{
+    console.log('sum using arrow func is :',a+b);
+};
+add(2,3);
---
+++ 22-01/loops.js
@@ -0,0 +1,55 @@
+// 1.
+
+// let count = 0;
+// for (let a = 1; a <= 100; a++) {
+//   if (a % 2 == 0) {
+//     console.log(a);
+//     count++;
+//   }
+// }
+// console.log('Number of even numbers are :',count);
+
+
+
+
+// 2.
+
+// let a = 1;
+// while (a <= 100) {
+//     if (a % 2 == 0) {
+//         console.log(a);
+//     }
+//     a++;
+// }
+
+
+
+// 3.
+
+// let a=1;
+// do{
+//     if(a%2==0){
+//         console.log(a);
+//     }
+//     a++;
+// }
+// while(a<=100);
+
+
+// 4.
+
+// for (let num = 2; num <= 100; num++) {
+//     let isPrime = true;
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//         if (num % i === 0 ) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if (isPrime) {
+//         console.log(num);
+//     }
+// }
+
+
+
