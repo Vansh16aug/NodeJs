c021ac564c03b6f3103a85b994260068885daed9
Vansh16aug <kumarvansh16aug@gmail.com>
2024-01-22 10:50:00 +0530

    Practised strings

---
+++ 22-01/string.js
@@ -0,0 +1,60 @@
+// 1.
+
+// let a='I am a';
+// let b=' good boy';
+
+// console.log(a+b);
+
+
+// 2.
+
+// let MotivationKa14='If you fail never give up , because FAIL means \"First Attempt In Learning \"';
+
+// console.log(MotivationKa14);
+
+
+
+//3.
+
+// let huhu='vansh';
+// console.log(huhu.length);
+
+// 4.
+
+// let a='vansh';
+// console.log(a.toUpperCase());
+
+//5.
+
+// let yoyo='I Love programming';
+
+// console.log(yoyo.replace("programming", "Javascript programming"));
+
+
+// 6.
+
+// let s=" kushina ";
+// console.log(s.repeat(3));
+
+
+// 7.
+
+// let n='I am Kalvian';
+// let n2=n.split(" ");
+
+// console.log(n2);
+
+
+// 8.
+
+// let a="Time and Tides wait for none";
+// console.log(a.indexOf("Ti"));
+
+
+// 9.
+
+// let b="When life gives you lemons make lemonade";
+// console.log(b.includes("lemon"));
+
+
+// 10.
