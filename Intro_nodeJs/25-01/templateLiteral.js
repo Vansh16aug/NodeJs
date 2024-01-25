4ccbf13a3b9e6927b2cc798dba20e321832bfebc
Vansh16aug <kumarvansh16aug@gmail.com>
2024-01-25 10:03:44 +0530

    started with node js

---
+++ Intro_nodeJs/25-01/templateLiteral.js
@@ -0,0 +1,23 @@
+//ES5
+console.log("vansh");
+
+let a=5;
+let b=7;
+console.log('Sum is ='+(a+b)+" yoyo");
+
+console.log('Any newline characters inserted in the source are part of the template literalUsing normal strings, you would have to use the following syntax in order to get multi-line strings:');
+
+
+//ES6
+console.log(`vansh
+yfyf`);
+
+console.log(`sum is =${a+b} yoo`);
+
+console.log(`Any newline characters inserted in the source are part of the template literal.
+
+Using normal strings, you would have to use the following syntax in order to get multi-line strings:`);
+
+function adf(strings, ...values){
+
+}
