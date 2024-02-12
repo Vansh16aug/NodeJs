e84d3f5de707f1026629cf9d884e68bf94250ac9
Vansh16aug <kumarvansh16aug@gmail.com>
2024-02-12 10:48:20 +0530

    done

--- Intro_nodeJs/12-02/http.js
+++ Intro_nodeJs/12-02/http.js
@@ -7,17 +7,21 @@
 // server.listen(3000);
 
 
-const http=require("http");
-const server=http.createServer((req,res)=>{
-    console.log('Refreshed!!');
-    if(req.url==='/Contact'){
+const http = require("http");
+const server = http.createServer((req, res) => {
+    if(req.url == '/'){
+        res.end("This is home page . but its not only thing you can do here hihihifhvhfhchchhchrttttfh");
+    }
+    else if(req.url == '/Contact'){
         res.end("This is contact page");
     }
-    else if(req.url==='/admin'){
+    else if(req.url == '/admin'){
         res.end("This is admin page");
     }
     else{
-        res.end("This is home page");
+        res.writeHead(404, {"Content-type": "text/html"});
+        res.end("<h1>404 Page not found </h1>");
     }
+    console.log('Refreshed!!');
 });
 server.listen(3000);
