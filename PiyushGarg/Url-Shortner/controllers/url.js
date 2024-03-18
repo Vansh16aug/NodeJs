88dccbef85664c69e0cc7ccaa3e5541e2eb959a5
Vansh16aug <kumarvansh16aug@gmail.com>
2024-03-18 22:41:31 +0530

    connecting backend to front end

--- PiyushGarg/Url-Shortner/controllers/url.js
+++ PiyushGarg/Url-Shortner/controllers/url.js
@@ -4,6 +4,7 @@ const URL = require("../models/url");
 async function handlegenerateNewShortUrl(req,res){
     const body = req.body;
     if(!body.url) return res.status(400).json({error:"url is required"});
+
     const shortID=shortid();
 
     await URL.create({
@@ -11,7 +12,9 @@ async function handlegenerateNewShortUrl(req,res){
         redirectURL: body.url,
         visitHistory:[],
     });
-    return res.json({id: shortID})
+    return res.render('home',{
+        id: shortID
+    });
 }
 
 async function handleGetAnalytics(req,res){
--- PiyushGarg/Url-Shortner/index.js
+++ PiyushGarg/Url-Shortner/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDb } = require("./connect");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const URL = require("./models/url");
 
 const app = express();
@@ -10,9 +12,17 @@ connectToMongoDb("mongodb://localhost:27017/url-shortner").then(() =>
   console.log("Mongodb connected")
 );
 
+//ejs
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"))
+
+
+//middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
 app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
@@ -28,9 +38,12 @@ app.get("/:shortId", async (req, res) => {
       },
     }
   );
-  res.redirect(entry.redirectURL);
+  
+  if (entry && entry.redirectURL) {
+    res.redirect(entry.redirectURL);
+  } else {
+    res.status(404).send("URL not found");
+  }
 });
-
-
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
